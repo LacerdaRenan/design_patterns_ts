9ffe49da5809fa18aea5920bcc4ac1a151e25dec
Renan Lacerda <renanrodlacerda@gmail.com>
2023-10-31 12:55:33 -0300

    feat: singleton

--- src/creational/singleton/db/db_singleton.ts
+++ src/creational/singleton/db/db_singleton.ts
@@ -4,15 +4,15 @@ type User =  {
 }
 
 export class MyDatabaseClassic {
-  private static instance: MyDatabaseClassic | null = null;
+  private static _instance: MyDatabaseClassic | null = null;
   private users: User[] = [];
   
   private constructor() { }
 
   public static getInstance(): MyDatabaseClassic {
-    if (MyDatabaseClassic.instance === null) MyDatabaseClassic.instance = new MyDatabaseClassic();
+    if (MyDatabaseClassic._instance === null) MyDatabaseClassic._instance = new MyDatabaseClassic();
 
-    return MyDatabaseClassic.instance;
+    return MyDatabaseClassic._instance;
   }
 
   public addUser(user: User): void { this.users.push(user) }
